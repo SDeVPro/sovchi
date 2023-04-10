1f3f908fb9c80f6e45e5ad4976cc05263f0de66e
Your Name <you@example.com>
2023-04-10 12:17:04 +0500

    Nimadir o'zgarish

--- sovchi/views.py
+++ sovchi/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 def index(request):
     sovchi = Sovchi.objects.all()
     return render(request,'index.html',{'sovchi':sovchi})
+
+def re_post(request):
+    return HttpResponse("Nimadir post")
